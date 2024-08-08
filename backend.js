c818948b7514cfa9218413664bdfbe5eb6d0458d
Alexander Davidow <davidowalexander@gmail.com>
2024-08-08 19:11:00 -0400

    day 2 changes, intial website creation/basic buttons

---
+++ backend.js
@@ -0,0 +1,9 @@
+function githubRedirect() {
+    let newUrl = "https://github.com/apdavidow";
+    window.location.href = newUrl;
+}
+
+function linkdlnRedirect() {
+    let newUrl = "https://linkedin.com/in/alexander-davidow-983b21240";
+    window.location.href = newUrl;
+}
